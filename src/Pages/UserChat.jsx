c56c90be129d1de2578f02991697451a6aec12f6
Priyanka <varmapriyanka2795@gmail.com>
2022-02-23 18:03:23 +0530

    fetch users api

--- src/Pages/UserChat.jsx
+++ src/Pages/UserChat.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react'
-import { Container, Row, Col, Form, Button, InputGroup, Offcanvas, ListGroup } from 'react-bootstrap'
+import { Row, Col, Form, Button} from 'react-bootstrap'
 import { FaPaperPlane } from 'react-icons/fa';
-import { FaSearch, FaGrin } from 'react-icons/fa';
+import {  FaGrin } from 'react-icons/fa';
 function UserChat() {
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    
     return (
         <div>
 
@@ -20,63 +17,7 @@ function UserChat() {
                     <p className='chatwith'>Chat with</p>
                     <p className='username'>Username</p>
                 </div>
-                <InputGroup className="p-3 d-none">
-                    <Button variant="secondary" size="md" active className='broker_btn' onClick={handleShow}>View All Brokers</Button>
-                    <Offcanvas show={show} onHide={handleClose}>
-                        <Offcanvas.Header closeButton>
-                            <Offcanvas.Title>
-                                <p>All Brokers </p>
-                            </Offcanvas.Title>
-                        </Offcanvas.Header>
-                        <Offcanvas.Body>
-                            <InputGroup className="px-2">
-                                <Form.Control type="search" placeholder="Search..." />
-                                <Button variant="primary" size="md" active>
-                                    <FaSearch />
-                                </Button>
-
-                            </InputGroup>
-
-                            <div className="user_list pt-3">
-                                <ListGroup variant="">
-                                    <ListGroup.Item>
-                                        <div className="users">
-                                            <div className="user_img">
-                                                <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="" width="45px" /><span>Username</span>
-                                            </div>
-
-                                        </div>
-                                    </ListGroup.Item>
-                                    <ListGroup.Item>
-                                        <div className="users">
-                                            <div className="user_img">
-                                                <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="" width="45px" /><span>Username</span>
-                                            </div>
-
-                                        </div>
-                                    </ListGroup.Item>
-                                    <ListGroup.Item>
-                                        <div className="users">
-                                            <div className="user_img">
-                                                <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="" width="45px" /><span>Username</span>
-                                            </div>
-
-                                        </div>
-                                    </ListGroup.Item>
-                                    <ListGroup.Item>
-                                        <div className="users">
-                                            <div className="user_img">
-                                                <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="" width="45px" /><span>Username</span>
-                                            </div>
-
-                                        </div>
-                                    </ListGroup.Item>
-                                </ListGroup>
-                            </div>
-                        </Offcanvas.Body>
-                    </Offcanvas>
-                    <Button variant="primary" size="md" active className='d-none'>View on fabric</Button>
-                </InputGroup>
+            
             </div>
 
             <div className="chat_room">
@@ -113,34 +54,16 @@ function UserChat() {
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum provident adipisci ad possimus error nesciunt ipsum ut maxime ipsa voluptatum, libero accusantium nihil minus voluptates nulla doloribus deserunt! Molestias, neque!</p>
                         </div>
                     </div>
-                    {/* <div class="message">
-                        <div class="blurb">Lorem ipsum dolor sit amet? </div>
-                    </div>
-                    <div class="message">
-                        <div class="odd-blurb">lorem ipsum dolor sit amet</div>
-                    </div> */}
+                   
                 </div>
 
 
 
             </div>
 
-            {/* <Row>
-                <Col lg={12}>
-                <div className="message_input">
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                  <Form.Control type="text" placeholder="Write new message.." />
-                </Form.Group>
-                <div className="msg_icon">
-                  
-                </div>
-              </div>
-                </Col>
-              </Row> */}
+         
             <div class="chat-bar">
-                {/* <span class="text-container">
-                    <span class="editable" contenteditable="true" data-text="Write new message"></span>
-                </span> */}
+              
                 <Row>
                     <Col lg={12} md={12} sm={12} xs={12} >
                         <div className="msg_ins d-flex">
